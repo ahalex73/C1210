cba722399c5ef72de03680bdf9cc90dcf4954ea1
Alexander Holmes <ahalex73@yahoo.com>
2023-02-16 16:00:24 -0500

    Adds Excercise 8, TODO: Fix broken loop - Hangs when running instead of printing out calculated modulo.

--- Assignment_4/ex8.c
+++ Assignment_4/ex8.c
@@ -10,7 +10,31 @@
 
 int main()
 {
+    int quit;
+    quit = 1;   // Our flag to jump out of the loop
+    
+    int first_operand;     // The user's desired First operand
+    int second_operand;    // The user's desired Second operand
+    int calculated_modulo; // The computer modulo operation based on users
+                           // First and second operand.
 
+    printf("This program computes moduli.\n");
+    printf("\nEnter an integer to server as the second operand: ");
+    scanf("%d", &second_operand);
+    printf("Now enter the first operand: ");
+    scanf("%d", &first_operand);
 
+    while (quit != 0)
+        calculated_modulo = (first_operand % second_operand); 
+        printf("%d %d is %d", first_operand, second_operand, calculated_modulo);
+
+        printf("\nEnter next number for first operand (<=0 to quit): ");
+        scanf("%d", &first_operand);
+
+        if (second_operand == 0)
+        {
+            quit = 0;
+        }
+            
     return 0;
 }
