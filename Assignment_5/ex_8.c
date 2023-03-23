92a5897db8d96d3a3f3b95aafedffd2cc0b6a59d
Alexander Holmes <ahalex73@yahoo.com>
2023-03-23 15:57:13 -0400

    Adds assignment 5

---
+++ Assignment_5/ex_8.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Programmer: Alexander Holmes
+// 3/23/2023
+// Intro to C Instructor: James Rhodes
+
+// This program  asks the user to enter the number of days and then converts that
+// value to weeks and days.
+
+int print_display();
+
+int main()
+{
+    print_display();
+
+
+    return 0;
+}
+
+
+int print_display(){
+    printf("Printing");
+
+    return 0;
+}
