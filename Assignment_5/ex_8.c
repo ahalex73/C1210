50035e48ad0d9d0748d70dac5d81e80ad78b43f1
Alexander Holmes <ahalex73@yahoo.com>
2023-03-23 23:59:03 -0400

    TODO: Fix input validation for choice selection TODO: Create gather number function to grab two operands.

--- Assignment_5/ex_8.c
+++ Assignment_5/ex_8.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Programmer: Alexander Holmes
 // 3/23/2023
@@ -8,19 +9,78 @@
 // This program  asks the user to enter the number of days and then converts that
 // value to weeks and days.
 
-int print_display();
+ char get_choice(void);
+ char get_first_character(void);
+ int get_int(void);
+ void count(void);
+ int main(void){
 
-int main()
-{
-    print_display();
+    int choice;
+    void count(void);
+    while ( (choice = get_choice()) != 'q'){
 
+        switch (choice){ 
+            case 'a' : printf("Selected Addition!\n");
+            break;
+            case 's' : printf("Selected Subtraction!\n");
+            break;
+            case 'm' : printf("Selected Multiplication!\n");
+            break;
+            case 'd' : printf("Selected Division!\n");
+            break;
+            default : printf("Program error!\n");
+            break;
+        }
+    }
+
+    printf("Bye.\n");
 
     return 0;
+ }
+
+char get_choice(void){
+    int ch;
+
+    printf("Enter the letter of your choice:\n");
+    printf("a. add          s. subtract\n" \
+           "m. multiply     d. divide\n" \
+           "q. quit\n");
+    ch = get_first_character();
+
+    while ((ch < 'a' || ch > 'm') && ch != 'q'){
+
+        printf("Please respond with a, b, c, or q.\n");
+        ch = get_first_character();
+    }
+
+    return ch;
+
 }
 
+char get_first_character(void){
+    int ch;
 
-int print_display(){
-    printf("Printing");
+    ch = getchar();
 
-    return 0;
+    while (getchar() != '\n')
+        continue;
+
+    return ch;
 }
+
+int get_int(void){
+
+    int input;
+    char ch;
+
+    while (scanf("%d", &input) != 1){
+
+        while ((ch = getchar()) != '\n')
+            putchar(ch); // dispose of bad input
+        printf(" is not an integer.\nPlease enter an ");
+        printf("integer value, such as 25, -178, or 3: ");
+    }
+
+    return input;
+
+} 
